81799f56c8d96c9175f9a81a3ccb7170bf74f711
Andrey Nosenko <a1996y@gmail.com>
2025-03-12 00:20:35 +0300

    update commands, charset

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 @echo off
-setlocal
+chcp 65001 > nul
+cls
 
 
 set "ip=192.168.1.5"
@@ -18,30 +19,64 @@ if errorlevel 1 (
 
 :menu
 cls
+echo "   ____           _          ____       _                    _____ ____  ";
+echo "  / ___| ___  ___| |_   _   / ___| __ _| | __ ___  ___   _  | ____| ___| ";
+echo " | |  _ / _ \/ _ \ | | | | | |  _ / _` | |/ _` \ \/ / | | | |  _| |___ \ ";
+echo " | |_| |  __/  __/ | |_| | | |_| | (_| | | (_| |>  <| |_| | | |___ ___) |";
+echo "  \____|\___|\___|_|\__, |  \____|\__,_|_|\__,_/_/\_\\__, | |_____|____/ ";
+echo "                    |___/                            |___/               ";
 echo ==========================================
 echo          ADB Script Menu
 echo ==========================================
-echo 1. Run adb_test.bat
-echo 2. Run backupzip.bat
-echo 3. Run backup.bat
-echo 4. Run restorezip.bat
-echo 5. Exit
+echo Что вы хотите сделать?
+echo 1. Проверка подключения к головному устройству по проводу.
+echo 2. Run adb_test.bat
+echo 3. Run backupzip.bat
+echo 4. Run backup.bat
+echo 5. Run restorezip.bat
+echo 6. Run ADB Console
+echo 7. Установка стартового пакета приложений.
+echo 0. Выйти
 echo ==========================================
-set /p choice="Please select an option (1-5): "
+set /p choice="Выберите действие для запуска (1-7): "
 
 if "%choice%"=="1" (
+    cd backup
+    adb devices -l | find "device usb" > nul 2>&1
+if %errorlevel%==0 (
+    echo Машина подключена
+    ) else (
+    echo Проверьте подключение к авто
+    )
+pause
+cls
+goto :menu
+)
+
+
+if "%choice%"=="2" (
     call adb_test.bat
-) else if "%choice%"=="2" (
-    call backupzip.bat
 ) else if "%choice%"=="3" (
-    call backup.bat
+    call backupzip.bat
 ) else if "%choice%"=="4" (
-    call restorezip.bat
+    call backup.bat
 ) else if "%choice%"=="5" (
-    echo Exiting...
+    call restorezip.bat
+) else if "%choice%"=="6" (
+    cd backup
+    cmd
+) else if "%choice%"=="7" (
+    cd ..\apps_to_install
+    for %%A in (*.apk) do (
+        echo Установка приложений: %%~nxA
+        adb install -g "%%A"
+    )
+    cd ..\backup  rem Возвращаемся в папку backup после установки
+  ) else if "%choice%"=="0" (
+    echo Выход из программы
     exit /b
 ) else (
-    echo Invalid choice. Please select a valid option.
+    echo Некорректный выбор. Попробуйте снова.
 )
 
 pause
